6aff2e9abe36389b8f4c3e540edda08814c70099
Wookyung <namwookyung.dev@gmail.com>
2023-10-05 16:47:54 +0900

    first commit_23.10.05

---
+++ app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "root4321",
+  database: "variety",
+  port: "3306"
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.json({message: "Hello World! Get!"});
+});
+
+app.post("/", (req, res, next) => {
+  res.json({message: "Hello World! Post!"});
+});
+
+// require("./app/routes/login/login.routes.js")(app);
+
+// Port Setting
+app.listen(3001, () => {
+  console.log("Server is running on port 3001!");
+});
+
+// TO DO : 암호화 작업, 기타 API 작업, 사용자별 권한 관리, Transacrion 관리 
